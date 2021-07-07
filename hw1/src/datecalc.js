db22557402148056d31ba0ff9bfb398dee015c19
Dmitriy <idem84@me.com>
2021-07-07 16:04:10 +0300

    update files, fixes bugs

--- hw1/src/datecalc.js
+++ hw1/src/datecalc.js
@@ -1,20 +1,50 @@
 import { DateTime } from "./luxon/luxon.js"; // 1
 
-export function diffDates(firstDate, secondDate) {
-    firstDate = DateTime.fromISO(firstDate);
-    secondDate = DateTime.fromISO(secondDate);
+export class DateCalc {
+    constructor(formId, resultId) {
+        this.dateCalcForm = document.getElementById(formId);
+        this.dateCalcResult = document.getElementById(resultId);
 
-    if (firstDate > secondDate)
-        secondDate = [firstDate, firstDate = secondDate][0]; // 2
+        this.addEvent();
+    }
 
-    return secondDate.diff(firstDate, ['years', 'months', 'days']).toObject();
+    diffDates(firstDate, secondDate) {
+        firstDate = DateTime.fromISO(firstDate);
+        secondDate = DateTime.fromISO(secondDate);
+
+        if (firstDate > secondDate)
+            secondDate = [firstDate, firstDate = secondDate][0]; // 2
+
+        return secondDate.diff(firstDate, ['years', 'months', 'days']).toObject();
+    }
+
+    diffToHtml = diff => `
+        <span> 
+            ${diff.years ? 'Лет: ' + diff.years : ''} 
+            ${diff.months ? 'Месяцев: ' + diff.months : ''} 
+            ${diff.days ? 'Дней: ' + diff.days : ''}
+        </span>`;
+
+    handleCalcDates = event => {
+        event.preventDefault();
+
+        this.clearResult();
+
+        let { firstDate, secondDate } = event.target.elements;
+        firstDate = firstDate.value, secondDate = secondDate.value;
+
+        if (firstDate && secondDate) {
+            const diff = this.diffDates(firstDate, secondDate); // 3
+            this.dateCalcResult.innerHTML = this.diffToHtml(diff); // 4
+        } else
+            this.dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); // 5
+    }
+
+    addEvent = () => {
+        this.dateCalcForm.addEventListener("submit", this.handleCalcDates);
+    }
+
+    clearResult = () => {
+        this.dateCalcResult.innerHTML = "";
+    }
 }
-
-// 3
-export const diffToHtml = diff => `
-    <span> 
-        ${diff.years ? 'Лет: ' + diff.years : ''} 
-        ${diff.months ? 'Месяцев: ' + diff.months : ''} 
-        ${diff.days ? 'Дней: ' + diff.days : ''}
-    </span>
-`;
--- hw1/src/main.js
+++ hw1/src/main.js
@@ -1,57 +1,14 @@
-import { diffDates, diffToHtml } from "./datecalc.js";
+import { DateCalc } from "./datecalc.js";
 import { Tabs } from "./tabs.js";
 import { Timer } from "./timer.js";
+import { initTimer } from "./utils.js";
 
-const dateCalcForm = document.getElementById("datecalc");
-const dateCalcResult = document.getElementById("datecalc__result");
+// Date calculator
+new DateCalc("datecalc", "datecalc__result");
 
-dateCalcForm.addEventListener("submit", handleCalcDates);
-
-function handleCalcDates(event) {
-    dateCalcResult.innerHTML = "";
-    event.preventDefault();
-
-    let { firstDate, secondDate } = event.target.elements;
-    firstDate = firstDate.value, secondDate = secondDate.value;
-
-    if (firstDate && secondDate) {
-        const diff = diffDates(firstDate, secondDate); // 3
-        dateCalcResult.innerHTML = diffToHtml(diff); // 4
-    } else
-        dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); // 5
-}
-
+// Tabs
 Tabs();
 
-let timerForm = document.getElementById('timer').getElementsByTagName('button');
-const timer = new Timer();
-
-const doTimerAction = id => {
-    switch (parseInt(id)) {
-        case 1:
-            let seconds = document.getElementById('timer').getElementsByClassName('seconds')[0].value;
-            let deadline = new Date(Date.parse(new Date()) + seconds * 1000);
-            timer.initializeClock('countdown', deadline);
-            document.getElementById('countdown').style.display = 'block';
-
-
-            break
-        case 2:
-            timer.stopClock();
-
-            break;
-    }
-}
-
-for (let i = 0, len = timerForm.length; i < len; i++) {
-    timerForm[i].addEventListener('click', function (e) {
-        e.preventDefault();
-        doTimerAction(e.target.value);
-    })
-}
-
-// let deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-
-//let deadline = new Date(Date.parse(new Date()) + 5 * 1000);
-
-//initializeClock('countdown', deadline);
+// Timer
+const timer = new Timer("timer");
+initTimer(timer);
